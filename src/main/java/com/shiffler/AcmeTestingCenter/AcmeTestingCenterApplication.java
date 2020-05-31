5fde2c96b70faebed87936196278941f8594c390
Jason Shiffler <jasonshiffler@yahoo.com>
2020-05-31 16:02:41 -0500

    -Removed ConfigSchedule.java file and moved Annotation to Main Application file.

--- src/main/java/com/shiffler/AcmeTestingCenter/AcmeTestingCenterApplication.java
+++ src/main/java/com/shiffler/AcmeTestingCenter/AcmeTestingCenterApplication.java
@@ -2,10 +2,12 @@ package com.shiffler.AcmeTestingCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class AcmeTestingCenterApplication {
 
 	public static void main(String[] args) {
